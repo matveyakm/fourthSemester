2fa071a316eeed3aa1edf39e42570831b2959c33
Matvey Akimenko <102146670+matveyakm@users.noreply.github.com>
2026-04-23 17:52:34 +0300

    Update HW3/ListIndexOf/ListIndexOf.fs
    Co-authored-by: Alexander Bugaev <ax.bugaev@gmail.com>

--- HW3/ListIndexOf/ListIndexOf.fs
+++ HW3/ListIndexOf/ListIndexOf.fs
@@ -2,7 +2,7 @@
 // Copyright (c) matveyakm. All rights reserved.
 // </copyright>
 
-namespace ListIndexOf
+module ListIndexOf
 
 /// <summary>
 /// Finds the first position (0-based index) of the given value in the list.
@@ -11,12 +11,11 @@ namespace ListIndexOf
 /// <param name="value">The value to search for</param>
 /// <param name="list">The list to search in</param>
 /// <returns>Some index if found, None otherwise</returns>
-module ListIndexOf =
-    let findIndex value list =
-        let rec loop idx lst =
-            match lst with
-            | [] -> None
-            | head :: tail ->
-                if head = value then Some idx
-                else loop (idx + 1) tail
-        loop 0 list
+let findIndex value list =
+    let rec loop idx lst =
+        match lst with
+        | [] -> None
+        | head :: tail ->
+            if head = value then Some idx
+            else loop (idx + 1) tail
+    loop 0 list
