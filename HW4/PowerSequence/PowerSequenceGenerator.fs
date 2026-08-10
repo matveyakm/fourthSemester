1ba6740cef427287d792f4f97529ca95edbb4e25
matveyakm <matveyakm@gmail.com>
2026-08-10 23:38:47 +0300

    fix

--- HW4/PowerSequence/PowerSequenceGenerator.fs
+++ HW4/PowerSequence/PowerSequenceGenerator.fs
@@ -15,15 +15,15 @@ module PowerSequence.PowerSequenceGenerator
 /// </summary>
 /// <param name="n">Starting exponent.</param>
 /// <param name="m">Number of elements to generate.</param>
-/// <returns>List of powers of 2.</returns>
-let generatePowerSequence n m = 
+/// <returns>Ok with list of powers of 2, or Error if m is negative.</returns>
+let generatePowerSequence n m =
     if m < 0 then
-        failwith "Number of elements to generate must be a non-negative integer."
+        Error "Number of elements to generate must be a non-negative integer."
     else
         let startValue = 2.0 ** (float n)
         let rec buildSequence acc count =
-            if count > m then acc
+            if count = 0 then acc
             else
-                let current = if count = 0 then startValue else List.head acc * 2.0
-                buildSequence (current :: acc) (count + 1)
-        buildSequence [] 0 |> List.rev
+                let current = List.head acc * 2.0
+                buildSequence (current :: acc) (count - 1)
+        buildSequence [startValue] m |> List.rev |> Ok
--- HW4/PowerSequenceTests/PowerSequenceGeneratorTests.fs
+++ HW4/PowerSequenceTests/PowerSequenceGeneratorTests.fs
@@ -8,47 +8,42 @@ open NUnit.Framework
 open FsUnit
 open PowerSequence.PowerSequenceGenerator
 
+type SequenceResult = Result<float list, string>
+
 /// <summary>
 /// Tests basic functionality of generatePowerSequence function.
 /// </summary>
 [<Test>]
 let ``generatePowerSequence should return [2.0] for n=1, m=0`` () =
-    generatePowerSequence 1 0 |> should equal [2.0]
-
-/// <summary>
-/// Tests that sequence contains correct number of elements.
-/// </summary>
-[<Test>]
-let ``generatePowerSequence should return list of length m+1`` () =
-    generatePowerSequence 0 4 |> should haveLength 5
+    generatePowerSequence 1 0 |> should equal (Ok [2.0] : SequenceResult)
 
 /// <summary>
 /// Tests correctness of sequence starting from 2^0 = 1.
 /// </summary>
 [<Test>]
-let ``generatePowerSequence should return [1.0; 2.0; 4.0; 8.0] for n=0, m=3`` () =
-    generatePowerSequence 0 3 |> should equal [1.0; 2.0; 4.0; 8.0]
+let ``generatePowerSequence should return [1.0; 2.0; 4.0; 8.0; 16.0] for n=0, m=4`` () =
+    generatePowerSequence 0 4 |> should equal (Ok [1.0; 2.0; 4.0; 8.0; 16.0] : SequenceResult)
 
 /// <summary>
 /// Tests correctness of sequence starting from 2^3 = 8.
 /// </summary>
 [<Test>]
 let ``generatePowerSequence should return [8.0; 16.0; 32.0] for n=3, m=2`` () =
-    generatePowerSequence 3 2 |> should equal [8.0; 16.0; 32.0]
+    generatePowerSequence 3 2 |> should equal (Ok [8.0; 16.0; 32.0] : SequenceResult)
 
 /// <summary>
 /// Tests with larger exponents.
 /// </summary>
 [<Test>]
 let ``generatePowerSequence should return [1024.0; 2048.0; 4096.0; 8192.0; 16384.0] for n=10, m=4`` () =
-    generatePowerSequence 10 4 |> should equal [1024.0; 2048.0; 4096.0; 8192.0; 16384.0]
+    generatePowerSequence 10 4 |> should equal (Ok [1024.0; 2048.0; 4096.0; 8192.0; 16384.0] : SequenceResult)
 
 /// <summary>
 /// Tests with negative starting exponent (fractional values).
 /// </summary>
 [<Test>]
 let ``generatePowerSequence should return correct values for n=-1, m=2`` () =
-    generatePowerSequence -1 2 |> should equal [0.5; 1.0; 2.0]
+    generatePowerSequence -1 2 |> should equal (Ok [0.5; 1.0; 2.0] : SequenceResult)
 
 /// <summary>
 /// Tests that sequence elements are consecutive powers of 2.
@@ -56,5 +51,13 @@ let ``generatePowerSequence should return correct values for n=-1, m=2`` () =
 [<Test>]
 let ``generatePowerSequence should produce consecutive powers of 2`` () =
     let result = generatePowerSequence 5 5
-    let expected = [32.0; 64.0; 128.0; 256.0; 512.0; 1024.0]
+    let expected = Ok [32.0; 64.0; 128.0; 256.0; 512.0; 1024.0] : SequenceResult
     result |> should equal expected
+
+/// <summary>
+/// Tests that a negative number of elements returns an error.
+/// </summary>
+[<Test>]
+let ``generatePowerSequence should return Error for negative m`` () =
+    generatePowerSequence 3 -1
+    |> should equal (Error "Number of elements to generate must be a non-negative integer." : SequenceResult)
